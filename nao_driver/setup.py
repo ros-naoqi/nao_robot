5cae355a207915a160e4dc101058ccfd8050a5f3
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2013-08-13 17:57:30 +0200

    [nao_driver] Also install nao_driver.util Python module

--- nao_driver/setup.py
+++ nao_driver/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['nao_driver'],
+    packages=['nao_driver', 'nao_driver.util'],
     package_dir={'': 'src'})
 
 setup(**setup_args)
